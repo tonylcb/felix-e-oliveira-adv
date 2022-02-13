591bdf431c6fcdcdc2ca58b3ab2ad7e89a45b40e
Tony <tony.leonardo@frncubo.com>
2022-02-13 07:43:14 -0300

    fix: ajuste height do body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import { Footer } from '../components/Footer'
-import { Header } from '../components/Header'
-import '../styles/globals.scss'
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import Div100vh from "react-div-100vh";
+
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Div100vh>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
-  )
+    </Div100vh>
+  );
 }
 
-export default MyApp
+export default MyApp;
