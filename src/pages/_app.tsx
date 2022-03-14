72e7b2d8eb90612602490df13e15514c131a583a
Tony <tony.leonardo@frncubo.com>
2022-03-14 11:12:36 -0300

    fix: ajuste fullbanners slider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import Div100vh from "react-div-100vh";
