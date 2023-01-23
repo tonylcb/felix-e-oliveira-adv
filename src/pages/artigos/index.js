1d848385fa032437bc8412005f26acda197ac6da
Tony <tony.leonardo@frncubo.com>
2023-01-23 09:23:46 -0300

    feat: main article page

--- src/pages/artigos/index.js
+++ src/pages/artigos/index.js
@@ -2,28 +2,29 @@ import styles from "./artigos.module.scss";
 import { createClient } from "../../../prismicio";
 
 export default function Artigos({ page }) {
-
+  console.log('page :>> ', page);
   return (
     <main className={styles.container}>
-      <div className={styles.content}>
-        <h1>Artigos</h1>
-        <section className={styles.articlesContainer}>
-          {page.map((article, i) => (
-            <>
-              <div key={i} className={styles.articleContainer}>
+      <h1 className="title">Artigos</h1>
+      <section className={styles.articlesContainer}>
+        {page.map((article, i) => (
+          <>
+            <a key={i} className={styles.articleContainer} href={`/artigos/${article.slugs[0]}`}>
+              <img src={article.data.slices[0].primary.articleImage.url} alt={article.data.slices[0].primary.articleImage.alt} />
+              <div className={styles.articleDescriptionContainer}>
                 <h3 className={styles.articleTitle}>{article.data.maintitle[0].text}</h3>
+                <p>{article.data.slices[0].primary.articleDescription[0].text}</p>
               </div>
-            </>
-          ))}
-        </section>
-      </div>
+            </a>
+          </>
+        ))}
+      </section>
     </main>
   );
 }
 
 export async function getStaticProps({ previewData }) {
   const client = createClient({ previewData });
-  console.log('client :>> ', client);
   const page = await client.getAllByType("articles");
 
   return {
